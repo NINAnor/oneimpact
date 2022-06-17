b6c85bd51c5cc4078573c499017c631500a5ad2c
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2022-06-17 11:34:11 +0200

    updated NAMESPACE removing multifit functions

--- R/multifit_single_multivar.R
+++ R/multifit_single_multivar.R
@@ -1,15 +1,26 @@
 #' Evaluation of single covariate effects at multiple scales for multiple covariates
 #'
-#' @param covariates `[character]` \cr List of names of the covariates to be tested, or a pattern
-#' within their names that differentiate among them.
+#' @param covariates `[character]` \cr List of names of the covariates to be tested,
+#' or a pattern within their names that differentiate among them.
 #'
 #' @export
-multifit_single_multivar <- function(mod, covariates, data, formula = NULL, args = NULL, criterion = "AIC", print_best = 5, ...) {
+multifit_single_multivar <- function(mod,
+                                     covariates,
+                                     data,
+                                     formula = NULL,
+                                     args = NULL,
+                                     criterion = "AIC",
+                                     corr_check = TRUE,
+                                     corr_criterion = c("usdm::vifcor",
+                                                        "usdm::vifstep")[1],
+                                     corr_threshold = 0.7,
+                                     print_best = 5, ...) {
 
 
   multifits <- list()
   model_comparison <- list()
   best_model <- list()
+  model_elements <- list()
 
   # For each covariate
   for(i in 1:length(covariates)) {
@@ -52,17 +63,48 @@ multifit_single_multivar <- function(mod, covariates, data, formula = NULL, args
         dplyr::arrange(dplyr::desc(!!dplyr::sym(criterion)))
     }
 
-    # add model call, warnings, and messages
-    calls <- lapply(multifit_var$models, function(x) x$call)
+    # get coefficients
+    coefs <- purrr::map2(
+      multifit_var$models,
+      multifit_var$lands_summary$spatial_scale,
+      function(x, y) {
+        coefs <- x$coefficients
+        unname(coefs[match(y, names(coefs))])
+      }) %>% unlist()
 
+    # get correlation between variables
+    if(corr_check) {
+      corr <- purrr::map2(
+        multifit_var$models,
+        multifit_var$summary$AIC,
+        function(x, y) {
+          if(!is.na(y)) {
+            m_vars <- all.vars(x$formula)[-1]
+            m_vars <- names(dplyr::select(data[, m_vars], where(is.numeric)))
+            corr_m <- do.call(eval(parse(text = corr_criterion)), list(x = data[, m_vars], th = corr_threshold))
+          } else {
+            corr_m <- NA
+          }
+          corr_m
+        })
+    } else
+      corr <- NULL
+
+    # get model formula
+    formulas <- lapply(multifit_var$models, function(x) x$formula)
+
+    # add formula, coefficients, correlation, warnings, and messages
     model_comparison[[i]] <- model_comparison_df %>%
       tibble::as_tibble() %>%
-      dplyr::mutate(call = calls[model_comparison_df$rowid],
-                    warnings = multifit_var$warnings[model_comparison_df$rowid],
-                    messages = multifit_var$messages[model_comparison_df$rowid])
+      dplyr::mutate(beta = coefs[model_comparison_df$rowid])
+
+    model_elements[[i]] <- list(corr = corr[model_comparison_df$rowid],
+                                formula = formulas[model_comparison_df$rowid],
+                                warnings = multifit_var$warnings[model_comparison_df$rowid],
+                                messages = multifit_var$messages[model_comparison_df$rowid])
 
     # get best model
-    multifit_var$models[[model_comparison_df$rowid[1]]]
+    best_model[[i]] <- multifit_var$models[[model_comparison_df$rowid[1]]]
   }
 
   # top models of each variable in a single
@@ -76,12 +118,18 @@ multifit_single_multivar <- function(mod, covariates, data, formula = NULL, args
     dplyr::bind_rows() %>%
     tibble::as_tibble()
 
-  best_model <- lapply()
-
   # rename elements
   names(multifits) <- covariates
   names(model_comparison) <- covariates
+  names(best_model) <- covariates
+  names(model_elements) <- covariates
 
   # return all lists
-  list(fits = multifits, model_comparison = model_comparison, best_model = best_model, model_comparison_all = model_comparison_all)
+  list(fits = multifits, model_comparison = model_comparison,
+       best_model = best_model,
+       model_elements = model_elements,
+       model_comparison_all = model_comparison_all)
 }
+
+
+grep
