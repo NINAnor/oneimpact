7bc94fc73e4dcc3127f678f7c6b2fbcf26217646
Benrnardo Niebuhr <bernardo_brandaum@yahoo.com.br>
2022-08-24 03:06:42 +0200

    created first version of pkgdown page

---
+++ data-raw/02_prepare_website.R
@@ -0,0 +1,15 @@
+# build pkgdown website
+
+# Run once to configure package to use pkgdown
+usethis::use_pkgdown()
+
+# Run to build the website
+pkgdown::build_site()
+
+# publish
+usethis::use_pkgdown_github_pages()
+
+#-----------
+# Update articles
+pkgdown::build_articles()
+
