79a4c8a57555a1eac35fa452b617bf45d2c2af44
Benrnardo Niebuhr <bernardo_brandaum@yahoo.com.br>
2022-08-25 13:06:34 +0200

    plot_zoi1d: changed parameter zoi_metric in examples

--- examples/plot_zoi1d_example.R
+++ examples/plot_zoi1d_example.R
@@ -1,22 +1,21 @@
 # one point, exponential decay
 plot_zoi1d(0, radius = 3, zoi_limit = 0.05,
-           fun = exp_decay, range_plot = c(0, 5), cumulative = FALSE)
+           fun = exp_decay, range_plot = c(0, 5), zoi_metric = "nearest")
 
 # exponential decay
 points <- c(0, 4.5, 7, 7.5)
-plot_zoi1d(points, radius = 3, fun = exp_decay, range_plot = c(0, 12), cumulative = FALSE)
-plot_zoi1d(points, radius = 3, fun = exp_decay, range_plot = c(0, 12), cumulative = TRUE)
+plot_zoi1d(points, radius = 3, fun = exp_decay, range_plot = c(0, 12), zoi_metric = "nearest")
+plot_zoi1d(points, radius = 3, fun = exp_decay, range_plot = c(0, 12), zoi_metric = TRUE)
 
 # threshold
-plot_zoi1d(points, radius = 3, fun = threshold_decay, range_plot = c(0, 12), cumulative = F)
-plot_zoi1d(points, radius = 3, fun = "step_decay", range_plot = c(0, 12), cumulative = T)
+plot_zoi1d(points, radius = 3, fun = threshold_decay, range_plot = c(0, 12), zoi_metric = "nearest")
+plot_zoi1d(points, radius = 3, fun = "step_decay", range_plot = c(0, 12), zoi_metric = "cumulative")
 
 # linear
-plot_zoi1d(points, radius = 3, fun = bartlett_decay, range_plot = c(0, 12), cumulative = F)
-plot_zoi1d(points, radius = 3, fun = "linear_decay", range_plot = c(0, 12), cumulative = T,
-                 return_df = T)
+plot_zoi1d(points, radius = 3, fun = bartlett_decay, range_plot = c(0, 12), zoi_metric = "nearest")
+plot_zoi1d(points, radius = 3, fun = "linear_decay", range_plot = c(0, 12), zoi_metric = "cumulative")
 
 # gaussian
-plot_zoi1d(points, radius = 3, fun = gaussian_decay, range_plot = c(0, 12), cumulative = F)
-plot_zoi1d(points, radius = 3, fun = "half_norm_decay", range_plot = c(0, 12), cumulative = T,
-                 return_df = T)
+plot_zoi1d(points, radius = 3, fun = gaussian_decay, range_plot = c(0, 12), zoi_metric = "nearest")
+plot_zoi1d(points, radius = 3, fun = "half_norm_decay", range_plot = c(0, 12), zoi_metric = "cumulative",
+           return_df = T)
