a22f9e9e303b29db1067aafea2d81cf947919503
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-06-12 16:55:06 +0200

    extract_response_strata: changed name from extract_case_strata to response

--- R/extract_response_strata.R
+++ R/extract_response_strata.R
@@ -1,4 +1,4 @@
-#' Separates the reponse variable, the strata variable, and other variables in a statistical formula
+#' Separates the reponse variable, the strata variable (), and other variables in a statistical formula
 #'
 #' @param f Formula
 #' @param other_vars logical. If true, all the (other) explanatory variables are also
@@ -8,23 +8,34 @@
 #' (`strata`), and possibly all explanatory variables, if `other_vars = TRUE`.
 #'
 #' @examples
+#' # formula with strata
 #' f <- formula(use ~ exp1 + exp2 + exp1:exp2 + strata(id))
-#' extract_case_strata(f)
-#' extract_case_strata(f, other_vars = TRUE)
+#' extract_response_strata(f)
+#' extract_response_strata(f, other_vars = TRUE)
+#'
+#' # formula without strata
+#' f2 <- formula(use ~ exp1 + exp2 + exp1:exp2)
+#' extract_response_strata(f2, other_vars = TRUE)
 #'
 #' @export
-extract_case_strata <- function(f, other_vars = F) {
+extract_response_strata <- function(f, other_vars = F) {
+
     # create list of variables
     x <- list()
     # transform formula into terms
     tmp <- as.character(f)
     # get the response variable - case 1/0
-    x$case  <- tmp[2]
+    x$response  <- tmp[2]
 
     # get the variable that corresponds to strata
-    tmp <- substr(tmp[3], gregexpr("strata", tmp[3])[[1]][1]+7, nchar(tmp[3]))
-    tmp <- substr(tmp, 1, gregexpr(")", tmp)[[1]][1]-1)
-    x$strata <- tmp
+    tmp2 <- grepl("strata", tmp[3])
+    if(tmp2 == FALSE) {
+      x$strata <- ""
+    } else {
+      tmp <- substr(tmp[3], gregexpr("strata", tmp[3])[[1]][1]+7, nchar(tmp[3]))
+      tmp <- substr(tmp, 1, gregexpr(")", tmp)[[1]][1]-1)
+      x$strata <- tmp
+    }
 
     if (other_vars){
       tmp  <- gsub(paste0("strata(", x$strata), 'xx', as.character(f)[3], fixed=T)
