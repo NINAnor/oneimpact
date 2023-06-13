808dbaab6e4dcf9004ec43cc374c9cc22d7c1a89
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-06-13 17:59:02 +0200

    create_resamples: implemented spatial sampling with H0 (instead of set_validation_sampling) and random sampling with H0

--- R/create_resamples.R
+++ R/create_resamples.R
@@ -1,27 +1,71 @@
 #' Create samples for fitting, calibrating, and validating models
 #'
 #' The function creates samples of the data to be included for three different
-#' model blocks: train (fitting), validating, and testing models.
+#' model blocks: train (fitting), test (calibrate), and validate.
 #' By default, samples are created through bootstrapping, i.e. with replacement. This means
 #' data observations can be repeated within a given sample block, but observations included
 #' in one block are necessarily excluded from the other blocks (e.g. observations selected
-#' for test will be absent from train and validation blocks).
+#' for validation will be absent from train and test blocks).
 #'
 #' @param y a vector of outcomes.
 #' @param times the number of partitions or samples to create
-#' @param p a 3 element vector with the percentage of data that goes to training, validation, and test
+#' @param p a 3 element vector with the percentage of data that goes to training (H1), test (H2), and validation (H0)
+#' @param sp_strat Default is NULL. If not NULL, the result of spat_strat() should be used here.
+#' @param colH0 Column number or name to define the ids of the H0 level - the one with ecological meaning, e.g. individual,
+#' population, or study area, used for validating the predictions of the fitted model.
+#' @param H0setup Setup for the H0 (validate) block. Either "LAO" (Leave-a-small-bit-of-All-Out) or "LOO"
+#' (Leave-One-Out). For instance, if H0 corresponds to the animal populations, "LAO" will keep some observations
+#' of each and all populations in the H0 (validation) set.
 #' @param list logical - should the results be in lists (TRUE) or matrices with the number of rows equal to
 #' floor(p * length(y)) and times columns.
 #' @param replace whether to perform the bootstrap sampling with or without replacement (Default = TRUE).
 #'
 #' @examples
+#' # random sampling, no validation block H0
 #' y <- runif(200)
 #' samples <- create_resamples(y, p = c(0.4, 0.2, 0.2), times = 5)
 #' samples
 #'
+#' # with validation block H0
+#' data(reindeer)
+#' library(terra)
+#' library(amt)
+#'
+#' # random sampling, with validation block H0
+#' samples <- create_resamples(1:nrow(reindeer), times = 5,
+#'                             p = c(0.2, 0.2, 0.2),
+#'                             max_size_validation_blockH0 = 1000,
+#'                             colH0 = reindeer$original_animal_id,
+#'                             list = FALSE)
+#' samples
+#'
+#' # spatially stratified sampling, with validation block H0
+#' spst <- spat_strat(reindeer, coords = c("x", "y"), colH0 = "original_animal_id",
+#'                    all_cols = F)
+#' samples <- create_resamples(1:nrow(reindeer), times = 5,
+#'                             p = c(0.2, 0.2, 0.2),
+#'                             max_number_fit_blockH1 = 20,
+#'                             sp_strat = spst,
+#'                             colH0 = "blockH0")
+#' samples
+#' sum(is.na(samples$test[[1]]))
+#' sapply(samples$train, function(x) sum(is.na(x)))
+#' sapply(samples$test, function(x) sum(is.na(x)))
+#'
+#' # small number of blocks or too high p[1] might incur in errors
+#' samples <- create_resamples(1:nrow(reindeer), times = 10,
+#'                             max_number_fit_blockH1 = 3,
+#'                             sp_strat = spst,
+#'                             colH0 = "blockH0")
+#'
 #' @export
 create_resamples <- function (y, times = 10,
                               p = c(0.4, 0.2, 0.2),
+                              max_size_validation_blockH0 = 1000,
+                              max_number_fit_blockH1 = 15,
+                              sp_strat = NULL,
+                              colH0 = NULL,
+                              H0setup = c("LAO", "LOO")[1],
                               list = TRUE,
                               replace = TRUE)
 {
@@ -36,47 +80,248 @@ create_resamples <- function (y, times = 10,
   # 3 matrices, for train, test, and validate
   indexes <- lapply(N, function(n) matrix(0, ncol = times, nrow = n))
 
-  # sample
-  # first validate, then train and test, mutually exclusive
-  for(col in seq(times)) {
+  #-------------------------
+  # random sampling
+  if(is.null(sp_strat)) {
 
-    # validation
-    index_val <- seq(along = y)
-    val <- sort(sample(index_val, size = nrow(indexes[[3]]), replace = TRUE))
+    #-------------------------
+    # random sampling, no validation level H0
+    if(is.null(colH0)) {
+      # sample
+      # first validate, then train and test, mutually exclusive
+      for(col in seq(times)) {
 
-    # fitting
-    index_fit <- index_val |>
-      setdiff(val)
-    fit <- sort(sample(index_fit, size = nrow(indexes[[1]]), replace = TRUE))
+        # validate
+        index_val <- seq(along = y)
+        val <- sort(sample(index_val, size = nrow(indexes[[3]]), replace = TRUE))
 
-    # calibrating
-    index_cal <- index_fit |>
-      setdiff(fit)
-    cal <- sort(sample(index_cal, size = nrow(indexes[[2]]), replace = TRUE))
+        # train/fit
+        index_train <- index_val |>
+          setdiff(val)
+        train <- sort(sample(index_train, size = nrow(indexes[[1]]), replace = TRUE))
 
-    indexes[[1]][,col] <- fit
-    indexes[[2]][,col] <- cal
-    indexes[[3]][,col] <- val
-  }
+        # test/calibrate
+        index_test <- index_train |>
+          setdiff(train)
+        test <- sort(sample(index_test, size = nrow(indexes[[2]]), replace = TRUE))
 
+        indexes[[1]][,col] <- train
+        indexes[[2]][,col] <- test
+        indexes[[3]][,col] <- val
+      } # end of loop for times
+
+    } else {
+
+      #-------------------------
+      # random sampling, with level H0
+
+      # sample
+      # first validate, then train and test, mutually exclusive
+      for(col in seq(times)) {
+
+        # validate
+        index_val <- seq(along = y)
+
+        # (sample validation block from populations (i.e. blockH0))
+        # split by H0 level
+        blocksH0 <- split(index_val, colH0)
+        # LAO
+        # sample pts in each pop/level - controlling for too imbalanced levels using max_size_validation_blockH0
+        # size is also controlled by the proportion of the whole data set for validation, p[3]
+        validation_set <- lapply(blocksH0, function(x){
+          sample(x, size = min(max_size_validation_blockH0, length(x)), replace = replace)})
+        # sample only the proportion set for validation
+        val <- sort(sample(unname(unlist(validation_set)), size = N[3]))
+        if(length(val) > N[3]) {
+          val <- sort(sample(val, N[3]))
+        } else {
+          if(length(val) < N[3]) {
+            warning(paste0("The size of the validation set is smaller than intended; you should check the values of p[3] (", p[3]
+                           ,") and/or max_size_validation_blockH0 (", max_size_validation_blockH0, ").",
+                           "As a reference, the number of points assigned to validation is ", length(val), ", and the number of points wanted for validation is ", N[3], "."))
+          }
+        }
+
+        # train/fit
+        index_train <- index_val |>
+          setdiff(val)
+        train <- sort(sample(index_train, size = nrow(indexes[[1]]), replace = TRUE))
+
+        # test/calibrate
+        index_test <- index_train |>
+          setdiff(train)
+        test <- sort(sample(index_test, size = nrow(indexes[[2]]), replace = TRUE))
+
+        indexes[[1]][,col] <- train
+        indexes[[2]][,col] <- test
+        indexes[[3]][,col] <- val
+      } # end of loop for times
+      # To implement, add condition for LOO and LAO
+    } # end of condition for presence of H0
+  } # end of condition for random sampling
+
+  #-------------------------
+  # sapial sampling, with level H0
+
+  #-------------------------
+  # spatially stratified sampling
+  if(!is.null(sp_strat)) {
+
+    #-------------------------
+    # spatially stratified sampling, no validation level H0
+    if(is.null(colH0)) {
+      stop("Spatial sampling without a given level H0 to be implemented.")
+      # To implement, add condition for LOO and LAO
+    } else {
+      #-------------------------
+      # spatially stratified sampling, with validation level H0
+
+      # first validate, then train and test, mutually exclusive
+      for(col in seq(times)) {
+
+        #------------------
+        # This is LAO, must set LOO in an alternative first block
+
+        # validate
+        # (sample validation block from populations (i.e. blockH0))
+        # split by H0 level
+        blocksH0 <- split(sp_strat, sp_strat$blockH0)
+        # LAO
+        # sample pts in each pop/level - controlling for too imbalanced levels using max_size_validation_blockH0
+        # size is also controlled by the proportion of the whole data set for validation, p[3]
+        validation_set <- lapply(blocksH0, function(x){
+          sample(x$id, size = min(max_size_validation_blockH0, length(x$id)), replace = replace)})
+        # sample only the proportion set for validation
+        val <- sort(sample(unname(unlist(validation_set)), size = N[3]))
+        if(length(val) > N[3]) {
+          val <- sort(sample(val, N[3]))
+        } else {
+          if(length(val) < N[3]) {
+            warning(paste0("The size of the validation set is smaller than intended; you should check the values of p[3] (", p[3]
+                           ,") and/or max_size_validation_blockH0 (", max_size_validation_blockH0, ").
+                           As a reference, the number of points assigned to validation are", length(val), ", and the number of points wanted for validation are ", N[3], "."))
+          }
+        }
+
+        # table(validation_set, spatial$blockH0)
+
+        # set blocks H1 and H2 to be sampled for train/fit and test
+
+        # remove observations already included for validation
+        index_train_test <- sp_strat$id |>
+          setdiff(val)
+        # split by blockH1
+        sp_strat_train_test <- sp_strat[index_train_test,] # remove rows for validation
+
+        # set blocks H1 to be sampled
+        blocksH1 <- split(sp_strat_train_test, sp_strat_train_test$blockH1)
+        # sample pts in each block H1
+        # can we remove the limitation of number of blocks here?
+        blocksH1 <- blocksH1[sample.int(length(blocksH1), min(max_number_fit_blockH1, length(blocksH1)))]
+
+        # set blocks H2 within those H1 to be sampled
+        # check which blocksH2 are present in the selection H1 blocks above
+        all_blocksH2 <- as.numeric(as.character(unlist(lapply(blocksH1, function(x){
+          levels(as.factor(x$blockH2)) }))))
+
+        # assign H2 blocks to test/calibration, the rest is for fitting
+        # for each H1 block, sample 1 blockH2 for ctest/alibration
+        test_blocksH2 <- as.numeric(as.character(unlist(lapply(blocksH1, function(x){
+          sample(levels(as.factor(x$blockH2)), 1)})))) # we can change later for more than 1
+
+        # start by setting all H2 blocks in the selected H1 for fitting
+        train_set <- sp_strat_train_test[sp_strat_train_test$blockH2 %in% all_blocksH2 & !(sp_strat_train_test$blockH2 %in% test_blocksH2),]
+        # train/fitting set
+        if(nrow(train_set) > N[1]) {
+          train <- sort(sample(train_set$id, N[1]))
+        } else {
+          train <- sort(train_set$id)
+          if(length(train) < N[1]) warning("The size of the train/fitting set is smaller than intended; you should check and possibly decrease the value of p[1] and/or increase the value of max_number_fit_blockH1.")
+        }
+
+        # test/calibrate
+        # set sampled blocksH2 to calibration
+        test_set <- sp_strat_train_test[sp_strat_train_test$blockH2 %in% all_blocksH2 & (sp_strat_train_test$blockH2 %in% test_blocksH2),]
+        # train/fitting set
+        if(nrow(test_set) > N[2]) {
+          test <- sort(sample(test_set$id, N[2]))
+        } else {
+          test <- sort(test_set$id)
+          if(length(test) < N[2]) {
+            test <- c(test, rep(NA, N[2]))[1:N[2]]
+            #print(sum(is.na(test)))
+            warning("The size of the test/calibration set is smaller than intended; you should check and possibly decrease the value of p[2] and/or increase the value of max_number_fit_blockH1.")
+            warning("Replacing the missing test observations by NA.")
+          }
+        }
+
+        #subsample fitting data for increased speed
+        # fitting_set <- sample(na.omit(spStrat$id[spStrat$set=="fitting"]), size=min(max_fit_size, sum(spStrat$set=="fitting", na.rm=T)))
+        # spStrat$set <- ifelse(spStrat$set == "fitting", NA, spStrat$set)
+        # spStrat$set <- ifelse(is.na(spStrat$set) & spStrat$id %in% fitting_set, "fitting", spStrat$set)
+
+        # check that no sample is empty
+        if(length(train) == 0) stop("No observations were assigned for model fitting.")
+        if(length(test) == 0) stop("No observations were assigned for model testing/calibration")
+        if(length(val) == 0) stop("No observations were assigned for model validation")
+
+        # update samples
+        indexes[[1]][,col] <- train
+        indexes[[2]][,col] <- test
+        indexes[[3]][,col] <- val
+      } # end of loop for times
+
+    } # end of condition for presence of H0
+  } # end of spatial stratification condition
+
+  # Organize the output
   if (list) {
     out <- lapply(indexes, function(x) {
       out_samp <- as.data.frame(x, stringsAsFactors = TRUE)
       attributes(out_samp) <- NULL
-      names(out_samp) <- pretty_seq(out_samp)
+      names(out_samp) <- oneimpact:::pretty_seq(out_samp)
       out_samp
     })
-    names(out) <- c("train", "validate", "test")
+    names(out) <- c("train", "test", "validate")
   }
   else {
     out <- lapply(indexes, function(x) {
       out_samp <- x
-      colnames(out_samp) <- pretty_seq(1:ncol(out_samp))
+      colnames(out_samp) <- oneimpact:::pretty_seq(1:ncol(out_samp))
       out_samp
     })
-    names(out) <- c("train", "validate", "test")
+    names(out) <- c("train", "test", "validate")
   }
   out
+}
+
+#' Set sampling parameters
+#'
+#' This function sets the sampling parameters.
+#'
+#' @param max_validation_size_blockH0 = 1000,
+#' @param max_number_fit_blockH1 Default = 10. Maximum number of blocks at level H1,
+#' used for fitting the model. Why do we set a maximum here?
+#' If all blocks should be used, use `max_number_fit_blockH1 = Inf`.
+#' @param max_fit_size 1000
+#'
+#' @returns List of sampling parameters for the spatial stratified cross-validation,
+#' with size of blocks of H0, H1, and H2.
+#'
+#' @examples
+#' set_sampling_params(1000, 10, 1000)
+set_sampling_params <- function(max_size_validate_blockH0 = 1000,
+                                max_number_fit_blockH1 = 10,
+                                max_size_fit_blockH1 = 1000) {
+
+  # Create list
+  sampling_params <- list()
+  # Set parameters
+  sampling_params$max_size_validate_blockH0 <- max_size_validate_blockH0
+  sampling_params$max_number_fit_blockH1 <- max_number_fit_blockH1
+  sampling_params$max_size_fit_blockH1 <- max_size_fit_blockH1
+
+  sampling_params
 }
 
 # Helper function to create resample names
