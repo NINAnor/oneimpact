c5be5e515a30b4c96da5351cf0b3a6da2e87ac50
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-06-15 13:28:07 +0200

    Added somersD and AUC to the namespace

--- R/concordance.R
+++ R/concordance.R
@@ -1,6 +1,7 @@
-#' Computes de conditional boyce index for model evaluation
+#' Computes the conditional Boyce index for model evaluation
 #'
-#' x: dataframe with x: predicted value, y:use vs. available, strat: stratum
+#' @param x A data.frame with three columns: x, the predicted values; y, the case variable
+#' (use vs. available, 1/0); and strat, the stratum.
 #'
 #' @export
 conditionalBoyce <- function(x,
@@ -10,10 +11,13 @@ conditionalBoyce <- function(x,
                              warnings = TRUE){
   # x: dataframe with x: predicted value, y:use vs. available, strat: stratum
   # https://cran.r-project.org/web/packages/survival/vignettes/concordance.pdf
-  lx <- split(x, x$strat)
 
+  # split by strata
+  lx <- split(x, x$strat)
+  # length of each strata
   rng <- range(unlist(lapply(lx, nrow)))
 
+  # test for problems in sampling
   test <- as.data.frame(table(unlist(lapply(lx, function(x){ which(x$y[order(x$x)]==1)}))))
   if (errors) {
     if (nrow(test) < 3) stop(paste0("Only ", nrow(test), " bins contain observations, the correlation is unreliable."))
@@ -23,6 +27,7 @@ conditionalBoyce <- function(x,
     if (nrow(test) < (rng[1]-1)) warning(paste0("Only ", nrow(test), " bins contain observations, the correlation is likely not reliable."))
   }
 
+  # compute correlation
   test2 <- data.frame(bins = c(1:rng[2]), frequency = 0)
   test2$frequency[match(as.character(test[,1]), as.character(test2$bins))] <- test$Freq
   b <- cor(test2$bins, test2$frequency, method = method)
@@ -30,8 +35,18 @@ conditionalBoyce <- function(x,
   return(b)
 }
 
-#' export
-concordanceSomers_d <- function(x){
+#' Computes concordance indices for model evaluation
+#'
+#' These functions compute different concordance indices.
+#'
+#' @references https://cran.r-project.org/web/packages/survival/vignettes/concordance.pdf
+#' @seealso [oneimpact::conditionalBoyce]
+#'
+#' @name concordance_indices
+#' @export
+somersD <- function(x,
+                    errors = TRUE,
+                    warnings = TRUE){
   #x: dataframe with x: predicted value, y:use vs. available, strat: stratum
   #https://cran.r-project.org/web/packages/survival/vignettes/concordance.pdf
   lx <- split(x, x$strat)
@@ -41,3 +56,18 @@ concordanceSomers_d <- function(x){
   d <- (C-D)/(C+D+Tx)
   return(d)
 }
+
+#' @rdname concordance_indices
+#' @export
+AUC <- function(x,
+                errors = TRUE,
+                warnings = TRUE) {
+  d <- somersD(x)
+  auc <- (d + 1)/2
+  auc
+}
+
+# Another possible function for presence/absence responses
+# Default cost for binomial outcomes in boot::cv.glm
+cost <- function(r, pi = 0, na.rm = TRUE) mean(abs(r-pi) > 0.5, na.rm = na.rm)
+
