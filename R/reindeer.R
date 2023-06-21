953a6b388b761fef61460b2499d3c2a060f8a065
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-06-21 15:24:16 +0200

    reindeer data: added reindeer_rsf with annotated data for rsf

--- R/reindeer.R
+++ R/reindeer.R
@@ -86,3 +86,47 @@
 #' data("reindeer_ssf")
 #'
 "reindeer_ssf"
+
+#' Annotated data of wild reindeer in Norway, prepared for point resource-selection analysis.
+#'
+#' @description
+#' A dataset containing GPS positions for wild reindeer (*Rangifer tarandus tarandus*) in
+#' Setesdal Austhei management area, southern Norway, organized in a use-availability design
+#' for point resource-selection analysis (RSA). It includes data from 9 female
+#' individuals collected between 2007 and 2010. The coordinates are presented in
+#' coordinate reference system ETRS89/UTM 33N (EPSG:25833).
+#' The data set was regularized with 3h data and 10 random locations were created for
+#' each used location. It was then annotated with rasters on the zone of influence
+#' of roads and cabins with radii 1000 - 3000, both at the start and the end
+#' point of a step.
+#'
+#' For more information on the original data, see ?[oneimpact::reindeer].
+#'
+#' @format A data frame with 31,735 rows and 28 variables:
+#' \describe{
+#'   \item{x}{GPS relocations expressed as UTM easting coordinates, in ETRS89/UTM 33N}
+#'   \item{y}{GPS relocations expressed as UTM northing coordinates, in ETRS89/UTM 33N}
+#'   \item{t}{Timestamp of GPS relocations, in UTC}
+#'   \item{original_animal_id}{Animal name assigned at captures}
+#'   \item{animal_year_id}{Unique individual identifier in the population, and sample unit- i.e. data from one animal in one year}
+#'   \item{sex}{Sex of the individual animal}
+#'   \item{to be completed}{to be completed}
+#' }
+#'
+#' @source Panzacchi, M., Van Moorter, B., Strand, O., Loe, L. E., & Reimers, E. (2015). Searching
+#' for the fundamental niche using individual-based habitat selection modelling across
+#' populations. Ecography, 38(7), 659–669. \url{https://doi.org/10.1111/ecog.01075}
+#'
+#' Cagnacci, F., Focardi, S., Ghisla, A., van Moorter, B., Merrill, E.H., Gurarie,
+#' E., Heurich, M., Mysterud, A., Linnell, J., Panzacchi, M., May, R., Nygård, T.,
+#' Rolandsen, C. and Hebblewhite, M. (2016), How many routes lead to migration?
+#' Comparison of methods to assess and characterize migratory movements. J Anim Ecol,
+#' 85: 54-68. \url{https://doi.org/10.1111/1365-2656.12449}.
+#'
+#' @usage data(reindeer_rsf)
+#'
+#' @examples
+#' library(tibble)
+#' data("reindeer_rsf")
+#'
+"reindeer_rsf"
--- data-raw/04_prepare_data_reindeer.R
+++ data-raw/04_prepare_data_reindeer.R
@@ -182,7 +182,7 @@ calc_zoi_nearest(cabins_presence, type = "euclidean")
 
 
 #------------------------------------
-# Annotated reindeer data
+# Annotated reindeer data for SSF
 library(terra)
 library(amt)
 library(dplyr)
@@ -250,6 +250,151 @@ plot(data_annotated_v[data_annotated_v$case_ == F], col = "grey", cex = 0.3)
 points(data_annotated_v[data_annotated_v$case_ == T], col = "red", cex = 0.3)
 
 # save
-reindeer_annotated <- data_annotated
-usethis::use_data(reindeer_annotated, overwrite = TRUE)
+reindeer_ssf <- data_annotated
+usethis::use_data(reindeer_ssf, overwrite = TRUE)
+# now we document by hand
+
+#------------------------------------
+# Annotated reindeer data for SSF
+library(terra)
+library(amt)
+library(dplyr)
+library(tidyr)
+data(reindeer)
+
+# use-availability setup for ssf
+r <- reindeer |>
+  amt::make_track(x, y, t, crs = 25833, all_cols = TRUE) |>
+  # dplyr::rename(x_ = x, y_ = y, t_ = t) |>
+  dplyr::filter(lubridate::month(t_) == 7) |> # only July
+  tidyr::nest(.by = animal_year_id) |>
+  dplyr::mutate(ua = purrr::map(data, function(x)
+    x |>
+      amt::track_resample(rate = hours(3), tolerance = hours(1)) |>
+      amt::filter_min_n_burst() |>
+      amt::steps_by_burst(keep_cols = "start") |>
+      amt::random_steps())) |>
+  dplyr::select(-data) |>
+  tidyr::unnest(ua) |>
+  dplyr::mutate(step_id = paste(animal_year_id, burst_, step_id_, sep = "_"))
+
+# compute zoi
+reindeer_area_v <- terra::vect(system.file("vector/reindeer_area.gpkg", package = "oneimpact"))
+# extent <- terra::buffer(reindeer_area_v, max(r$sl_)*1.1) |>
+#   terra::ext()
+extent <- r |>
+  terra::vect(geom = c("x1_", "y1_")) |>
+  terra::buffer(max(r$sl_)*1.1) |>
+  terra::ext()
+ress <- 200
+nrows <- floor(range(extent)[2]/ress+1)
+ncols <- floor(range(extent)[1]/ress+1)
+rr <- terra::rast(extent, nrows = nrows, ncols = ncols)
+# cabins
+reindeer_cabins_v <- terra::vect(system.file("vector/reindeer_cabins.gpkg", package = "oneimpact"))
+reindeer_cabins <- terra::rasterize(reindeer_cabins_v, rr, fun = length)
+cabins_zoi <- calc_zoi_cumulative(reindeer_cabins, radius = c(seq(1000, 3000, 1000)),
+                                  type = "Gauss", zeroAsNA = TRUE)
+names(cabins_zoi) <- paste0("cabins", seq(1000, 3000, 1000))
+terra::plot(cabins_zoi)
+# roads
+reindeer_roads_v <- terra::vect(system.file("vector/reindeer_roads_public.gpkg", package = "oneimpact"))
+reindeer_roads <- terra::rasterize(reindeer_roads_v, rr, field = "value")
+roads_zoi <- calc_zoi_cumulative(reindeer_roads, radius = c(seq(1000, 3000, 1000)),
+                                 type = "Gauss", zeroAsNA = TRUE)
+names(roads_zoi) <- paste0("roads", seq(1000, 3000, 1000))
+plot(roads_zoi)
+
+# extract
+st <- terra::extract(c(cabins_zoi, roads_zoi), terra::vect(r, geom = c("x1_", "y1_")))
+names(st)[-1] <- paste0("start_", names(st)[-1])
+end <- terra::extract(c(cabins_zoi, roads_zoi), terra::vect(r, geom = c("x2_", "y2_")))
+names(end)[-1] <- paste0("end_", names(end)[-1])
+
+# anottated data
+data_annotated <- dplyr::bind_cols(r, st[-1], end[-1])
+data_annotated[grep("cabins|roads", names(data_annotated))[1]:ncol(data_annotated)] <-
+  apply(data_annotated[grep("cabins|roads", names(data_annotated))[1]:ncol(data_annotated)], 2, scale)
+data_annotated$case_ <- ifelse(data_annotated$case_, 1, 0)
+
+# plot
+data_annotated_v <- terra::vect(data_annotated, geom = c("x2_", "y2_"))
+plot(data_annotated_v[data_annotated_v$case_ == F], col = "grey", cex = 0.3)
+points(data_annotated_v[data_annotated_v$case_ == T], col = "red", cex = 0.3)
+
+# save
+reindeer_ssf <- data_annotated
+usethis::use_data(reindeer_ssf, overwrite = TRUE)
+# now we document by hand
+
+#------------------------------------
+# Annotated reindeer data for SSF
+# packages
+library(oneimpact)
+library(glmnet)
+library(terra)
+library(amt)
+data(reindeer)
+
+# use-availability setup for rsf
+r <- reindeer |>
+  amt::make_track(x, y, t, crs = 25833, all_cols = TRUE) |>
+  # dplyr::rename(x_ = x, y_ = y, t_ = t) |>
+  dplyr::filter(lubridate::month(t_) == 7)
+
+hr_buff1km <- reindeer |>
+  amt::make_track(x, y, t, crs = 25833, all_cols = TRUE) |>
+  amt::hr_mcp(level = 1)
+hr_buff1km$mcp <- sf::st_buffer(hr_buff1km$mcp, 1000)
+rp <- amt::random_points(hr_buff1km, n = nrow(r) * 10)
+
+# dataset
+r <- r |>
+  dplyr::mutate(case_ = TRUE) |>
+  dplyr::select(case_, x_, y_) |>
+  dplyr::bind_rows(rp)
+
+# compute zoi
+# reindeer_area_v <- terra::vect(system.file("vector/reindeer_area.gpkg", package = "oneimpact"))
+# extent <- terra::buffer(reindeer_area_v, max(r$sl_)*1.1) |>
+#   terra::ext()
+extent <- r |>
+  terra::vect(geom = c("x_", "y_")) |>
+  terra::buffer(3000) |>
+  terra::ext()
+ress <- 200
+nrows <- floor(range(extent)[2]/ress+1)
+ncols <- floor(range(extent)[1]/ress+1)
+rr <- terra::rast(extent, nrows = nrows, ncols = ncols)
+# cabins
+reindeer_cabins_v <- terra::vect(system.file("vector/reindeer_cabins.gpkg", package = "oneimpact"))
+reindeer_cabins <- terra::rasterize(reindeer_cabins_v, rr, fun = length)
+cabins_zoi <- calc_zoi_cumulative(reindeer_cabins, radius = c(seq(1000, 3000, 1000)),
+                                  type = "Gauss", zeroAsNA = TRUE)
+names(cabins_zoi) <- paste0("cabins", seq(1000, 3000, 1000))
+terra::plot(cabins_zoi)
+# roads
+reindeer_roads_v <- terra::vect(system.file("vector/reindeer_roads_public.gpkg", package = "oneimpact"))
+reindeer_roads <- terra::rasterize(reindeer_roads_v, rr, field = "value")
+roads_zoi <- calc_zoi_cumulative(reindeer_roads, radius = c(seq(1000, 3000, 1000)),
+                                 type = "Gauss", zeroAsNA = TRUE)
+names(roads_zoi) <- paste0("roads", seq(1000, 3000, 1000))
+plot(roads_zoi)
+
+# extract
+pts <- terra::extract(c(cabins_zoi, roads_zoi), terra::vect(r, geom = c("x_", "y_")))
+
+# anottated data
+reindeer_rsf <- dplyr::bind_cols(r, pts[-1])
+reindeer_rsf[grep("cabins|roads", names(reindeer_rsf))[1]:ncol(reindeer_rsf)] <-
+  apply(reindeer_rsf[grep("cabins|roads", names(reindeer_rsf))[1]:ncol(reindeer_rsf)], 2, scale)
+reindeer_rsf$case_ <- ifelse(reindeer_rsf$case_, 1, 0)
+
+# plot
+reindeer_rsf_v <- terra::vect(reindeer_rsf, geom = c("x_", "y_"))
+plot(reindeer_rsf_v[reindeer_rsf$case_ == F], col = "grey", cex = 0.3)
+points(reindeer_rsf_v[reindeer_rsf$case_ == T], col = "red", cex = 0.3)
+
+# save
+usethis::use_data(reindeer_rsf, overwrite = TRUE)
 # now we document by hand
