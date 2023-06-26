f07539ed7db5d262762221566e49ec4f5448a2da
Benrnardo Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-06-26 16:45:09 +0200

    fit_net_clogit: added pROC_AUC option, added filter for relevant covariates, added draft of bag_fir_net_clogit

--- R/fit_net_clogit.R
+++ R/fit_net_clogit.R
@@ -7,7 +7,7 @@
 fit_net_clogit <- function(f, data,
                            samples, i = 1,
                            kernel_vars = c("step_length", "ta"),
-                           metric = c(conditionalBoyce, somersD, AUC)[[1]],
+                           metric = c(conditionalBoyce, somersD, AUC, proc_AUC)[[1]],
                            out_dir_file = NULL,
                            na.action = "na.pass",
                            ...) {
@@ -22,10 +22,13 @@ fit_net_clogit <- function(f, data,
   # get variable referring to strata
   strat <- vars$strata
 
+  # relevant columns
+  all_vars <- all.vars(f)
+
   # separate data for fitting, calibration, and validation
-  train_data  <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$train[[i]]], ]
-  test_data <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$test[[i]]], ]
-  validate_data <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$validate[[i]]], ]
+  train_data  <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$train[[i]]], all_vars]
+  test_data <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$test[[i]]], all_vars]
+  validate_data <- data[data[[strat]] %in% data[data[[case]] == 1,][[strat]][samples$validate[[i]]], all_vars]
 
   # perform penalized regression with glmnet
   # use glmnet.cv?
@@ -67,10 +70,10 @@ fit_net_clogit <- function(f, data,
   test_pred_vals <- model.matrix(f2, test_data) %*% results$coef
 
   # save results
-  results$fit_score <- metric(data.frame(x = train_pred_vals,
-                                                   y = train_data[[wcols$response]],
-                                                   strat = train_data[[wcols$strata]]))
-  results$calibration_score <- max(d)
+  results$train_score <- metric(data.frame(x = train_pred_vals,
+                                         y = train_data[[wcols$response]],
+                                         strat = train_data[[wcols$strata]]))
+  results$test_score <- max(d)
 
   #----
   # Validation step
@@ -109,3 +112,83 @@ fit_net_ssf <- fit_net_clogit
 #' @rdname fit_net_clogit
 #' @export
 fit_net_issf <- fit_net_clogit
+
+#' Fit a bag of conditional logistic regression/SSF/iSSF models with penalized regression in a train-validate-test setup
+#'
+#' @param ... Options for net_logit and glmnet
+#' @param mc.cores Only relevant if `parallel == "mclapply"`. If `parallel == "foreach"`, cores must
+#' be assigned before running `fit_multi_net_logit()` using [parallel::makeCluster()] and
+#' [doParallel::registerDoParallel()].
+#'
+#' @name bag_fit_net_clogit
+#' @export
+bag_fit_net_clogit <- function(f, data,
+                               samples,
+                               kernel_vars = c("step_length", "ta"),
+                               metric = c(conditionalBoyce, somersD, AUC)[[1]],
+                               out_dir_file = NULL,
+                               na.action = "na.pass",
+                               parallel = c(FALSE, "foreach", "mclapply")[1],
+                               mc.cores = 2L,
+                               ...) {
+
+  # If there is parallel implementation with forach
+  if(parallel == "foreach") {
+    packs <- c("parallel", "foreach", "doParallel")
+    if(!any(packs %in% (base::.packages())))
+      warnings(paste0("Parallel fitting of the models using 'foreach' requires the packages ", paste(packs, collapse = ","),
+                      " to be loaded and cores to be assigned. Please check it."))
+    # check if cores were assigned
+    fittedl <- foreach::foreach(i = 1:length(samples$train),
+                                .packages = "oneimpact") %dopar% {
+                                  fit_net_clogit(f = f,
+                                                 data = data,
+                                                 samples = samples,
+                                                 i = i,
+                                                 kernel_vars = kernel_vars,
+                                                 metric = metric,
+                                                 out_dir_file = out_dir_file,
+                                                 na.action = na.action,
+                                                 ...)
+                                }
+  }
+
+  # If there is parallel implementation with forach
+  if(parallel == "mclapply") {
+    packs <- c("parallel")
+    if(!any(packs %in% (base::.packages())))
+      warnings(paste0("Parallel fitting of the models using 'mclapply' requires the packages ", paste(packs, collapse = ","),
+                      " to be loaded and cores to be assigned. Please check it."))
+    # check if cores were assigned
+    fitted_list <- parallel::mclapply(1:length(samples$train), mc.cores =  mc.cores, function(i) {
+      fit_net_clogit(f = f,
+                     data = data,
+                     samples = samples,
+                     i = i,
+                     kernel_vars = kernel_vars,
+                     metric = metric,
+                     out_dir_file = out_dir_file,
+                     na.action = na.action,
+                     ...)
+    })
+  }
+
+  # Common loop if parallel = FALSE
+  if(parallel == FALSE) {}
+  fitted_list <- list()
+  for(i in 1:length(samples$train)) {
+    fitted_list[[i]] <- fit_net_clogit(f = f,
+                                       data = data,
+                                       samples = samples,
+                                       i = i,
+                                       kernel_vars = kernel_vars,
+                                       metric = metric,
+                                       out_dir_file = out_dir_file,
+                                       na.action = na.action,
+                                       ...)
+  }
+
+  names(fitted_list) <- names(samples$train)
+  # define new class?
+  fitted_list
+}
