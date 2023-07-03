04c21709a0f093902b7f03c03f5e16abaf6eb499
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-07-03 07:37:30 +0200

    add_zoi_formula: added more elements to the output grid

--- R/add_zoi_formula.R
+++ R/add_zoi_formula.R
@@ -9,6 +9,8 @@
 #' @param type Shape(s) of the ZOI
 #' @param pattern Pattern to be replaced in the formula, corresponding to the ZOI radii or
 #' ZOI shapes and radii.
+#' @param cumulative = NULL. Possible: "cumulative", "nearest" (if this is to be set by the
+#' user, otherwise it should be taken from the variable name)
 #' @param separator Separator to be used between type and zoi_radius, when `type`
 #' is provided. Default is "_".
 #' @param grid logical. Whether or not the grid with all ZOI radii and shape values
@@ -24,8 +26,24 @@
 #' add_zoi_formula(f, zoi_radius = c(1000, 2000, 3000), type = "_exp_decay", pattern = "XXX")
 #' add_zoi_formula(f, zoi_radius = c(1000, 2000, 3000), type = c("_exp_decay", "_threshold"), pattern = "XXX")
 #'
+#' # grid
+#' f <- case_ ~ strata(step_id_) + land_use + sl_*startpt_roads_XXX + sl_*startpt_cabins_XXX
+#' add_zoi_formula(f, zoi_radius = c(1000, 2000, 3000), type = c("exp_decay", "threshold"), pattern = "XXX",
+#'                 grid = TRUE, cumulative = "nearest")
+#'
+#' # grid 2
+#' f <- case_ ~ strata(step_id_) + sl_*startpt_roads_cumulative_XXX + sl_*startpt_cabins_nearest_XXX
+#' add_zoi_formula(f, zoi_radius = c(1000, 2000, 3000), type = c("exp_decay", "threshold"), pattern = "XXX",
+#'                 grid = TRUE)
+#'
+#' # grid 3
+#' f <- case_ ~ strata(step_id_) + sl_*startpt_roads_XXX + sl_*startpt_cabins_XXX
+#' add_zoi_formula(f, zoi_radius = c(1000, 2000, 3000), type = c("nearest_exp_decay", "cumulative_exp_decay"), pattern = "XXX",
+#'                 grid = TRUE)
+#'
 #' @export
 add_zoi_formula <- function(f, zoi_radius, type = NULL, pattern = "XXX",
+                            cumulative = NULL,
                             separator = "_", grid = FALSE) {
 
   # separate case and strata from formula
@@ -51,18 +69,43 @@ add_zoi_formula <- function(f, zoi_radius, type = NULL, pattern = "XXX",
   # transform into formula
   f1 <- as.formula(f1)
 
+  # for variable names
+  # f_names <- as.formula(paste0(". ~ - 1 +", f4))
+  # all_covs <- all.vars(f_names)[-1]
+
   # output
-  out <- list(formula = f1, grid = NA)
+  out <- list(formula = f1, grid = grid)
   # fill grid
   if(grid) {
+    # get ZOI vals
     lines_zoi <- grep(pattern, grid_zoi$Var3)
     lines_other <- match(unique(grid_zoi$Var3), grid_zoi$Var3)
     lines_other <- lines_other[!(lines_other %in% lines_zoi)]
-    grid_zoi[lines_other,c(1,2)] <- NA
-    grid_zoi <- grid_zoi[c(lines_zoi, lines_other),][-3]
-    grid_zoi$variable <- f3
-    names(grid_zoi)[c(1,2)] <- c("zoi_radius", "shape")
-    out$grid <- grid_zoi
+    grid_zoi$is_zoi <- 1
+    grid_zoi[lines_other,c(1,2,4)] <- NA
+    grid_zoi <- grid_zoi[sort(c(lines_zoi, lines_other)),]
+    grid_zoi$is_zoi <- ifelse(is.na(grid_zoi$is_zoi), 0, grid_zoi$is_zoi)
+
+    # add variable in formula
+    #grid_zoi$variable_zoi <- all_covs#f3
+    grid_zoi$term_zoi <- f3
+    grid_zoi$Var3 <- sapply(grid_zoi$Var3, function(x, y){ gsub(y, "", x)}, y = pattern)
+    names(grid_zoi)[c(1,2,3)] <- c("zoi_radius", "shape", "variable")
+
+    # is cumulative?
+    if(!is.null(cumulative)) {
+      grid_zoi$cumulative <- ifelse(grid_zoi$is_zoi, cumulative, NA_character_)
+    } else {
+      grid_zoi$cumulative <- NA_character_
+      grid_zoi$cumulative <- ifelse(grepl("cumulative", grid_zoi$term_zoi), "cumulative", grid_zoi$cumulative)
+      grid_zoi$cumulative <- ifelse(grepl("nearest", grid_zoi$term_zoi), "nearest", grid_zoi$cumulative)
+    }
+    # remove cumulative from shape, if present
+    patt_cum <- "cumulative_|cumulative|_cumulative|_cumulative_|nearest|_nearest|nearest_|_nearest_"
+    grid_zoi$shape <- gsub(pattern = patt_cum, replacement = "", grid_zoi$shape)
+
+    out$grid <- grid_zoi[, c("is_zoi", "cumulative", "shape", "zoi_radius", "variable", #"variable_zoi",
+                             "term_zoi")]
   }
 
   return(out)
