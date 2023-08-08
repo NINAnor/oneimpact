0b51c9d62a96bbc1a0d401ea0a63f63b90f3afda
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2023-08-08 18:42:45 +0200

    bag_models: added score2weight_mean and inv_mean function, and the second as default. Added new output in case of errors in fitting models

--- R/bag_models.R
+++ R/bag_models.R
@@ -12,19 +12,15 @@
 #' @name bag_models
 #' @export
 bag_models <- function(fitted, data,
-                       score2weight = NULL,
+                       score2weight = score2weight_invmean,
                        weights_col = c("validation_score", "habitat_validation_score")[1],
                        score_threshold = 0.7,
-                       weights_function = NULL, out_dir){
+                       weights_function = NULL,
+                       out_dir){
 
   # function to transform scores to weights, if none is provided
   if (is.null(score2weight)){
-    score2weight <- function(x, col = "validation_score", score_threshold = 0.7){
-      x <- x[[weights_col]]
-      x <- 1/mean(1/x, na.rm = TRUE)
-      x <- ifelse(x < score_threshold, 0, x) #set poorly validated models to zero
-      return(x)
-    }
+    score2weight <- score2weight_invmean
   }
 
   # weighing function for scores
@@ -49,30 +45,55 @@ bag_models <- function(fitted, data,
   # method
   # assuming all models follow the same method, must be changed to lapply if not
   result$method <- fitted$method
+  # weights
+  result$weight_ref <- weights_col
+  # weight threshold
+  result$weight_threshold <- score_threshold
   # metric
   result$metric <- fitted$metric
+
+  # errors
+  result$errors <- err <- sapply(lres, function(x) class(x) == "try-error")
+  result$error_message <- sapply(1:length(err),
+                                 function(i) {
+                                   if(err[i]) return(lres[[i]]) else return(NA)
+                                 })
+  # number of errors
+  result$n_errors <- sum(err)
+  # final number of valid models
+  result$n_final <- result$n - result$n_errors
+  if(result$n_final == 1) {
+    warning("Only one model in the bag was succefully fitted. The results will be based on this single model.")
+  } else {
+    if(result$n_final < 1) {
+      stop("No models were succefully fitted. Please check and re-fit the models.")
+    }
+  }
 
   # should we unstandardize the coefs here!?!
 
   # synthesize results
-  result$coef <- do.call("cbind", lapply(lres, function(x) { x$coef } ))
-  colnames(result$coef) <- names(lres)
-  result$var_names <- lres[[1]]$var_names
-  result$fit_score <- do.call("cbind", lapply(lres, function(x) { x$fit_score} ))
-  result$calibration_score <- do.call("cbind", lapply(lres, function(x) { x$calibration_score} ))
-  result$validation_score <- do.call("cbind", lapply(lres, function(x) { x$validation_score} ))
-  result$habitat_validation_score <- do.call("cbind", lapply(lres, function(x) { x$habitat_validation_score} ))
+  result$coef <- do.call("cbind", lapply(lres[!err], function(x) { x$coef } ))
+  colnames(result$coef) <- names(lres[!err])
+  result$var_names <- lres[!err][[1]]$var_names
+  result$fit_score <- do.call("cbind", lapply(lres[!err], function(x) { x$fit_score} ))
+  result$calibration_score <- do.call("cbind", lapply(lres[!err], function(x) { x$calibration_score} ))
+  result$validation_score <- do.call("cbind", lapply(lres[!err], function(x) { x$validation_score} ))
+  result$habitat_validation_score <- do.call("cbind", lapply(lres[!err], function(x) { x$habitat_validation_score} ))
 
   # weights
-  result$weights <- unlist(lapply(lres, score2weight, col = weights_col, score_threshold = score_threshold))
+  result$weights <- unlist(lapply(lres[!err], score2weight, col = weights_col, score_threshold = score_threshold))
   result$weights <- weights_function(result$weights)
+  if(anyNA(result$weights)) {
+    warning("At least one resample has NaN weight. Please check the models' validation scores the the 'score_threshold' parameter.")
+  }
 
   # average validation scores
   if(nrow(result$validation_score) > 1) {
     result$validation_score_summary <- data.frame(min = apply(result$validation_score,2,min,na.rm = TRUE),
-                                                median = apply(result$validation_score,2,median,na.rm = TRUE),
-                                                mean = apply(result$validation_score,2,mean,na.rm = TRUE),
-                                                max = apply(result$validation_score,2,max,na.rm = TRUE))
+                                                  median = apply(result$validation_score,2,median,na.rm = TRUE),
+                                                  mean = apply(result$validation_score,2,mean,na.rm = TRUE),
+                                                  max = apply(result$validation_score,2,max,na.rm = TRUE))
   } else {
     result$validation_score_summary <- data.frame(median = apply(result$validation_score,2,mean,na.rm = TRUE))
   }
@@ -81,8 +102,8 @@ bag_models <- function(fitted, data,
 
   # weighted validation
   if(nrow(result$validation_score) > 1) {
-    result$avg_weighted_validation_score <- c(
-      apply(result$validation_score,2,min,na.rm = TRUE) %*% result$weights,
+    result$avg_weighted_validation_score <- cbind(
+      apply(result$validation_score,2,min, na.rm = TRUE) %*% result$weights,
       apply(result$validation_score,2,median,na.rm = TRUE) %*% result$weights,
       apply(result$validation_score,2,mean,na.rm = TRUE) %*% result$weights,
       apply(result$validation_score,2,max,na.rm = TRUE) %*% result$weights)
@@ -156,5 +177,23 @@ w_strech_max_squared <- function(x){
   x <- x/max(x, na.rm = T)
   x <- x^2
   x <- x/sum(x, na.rm = T)
+  return(x)
+}
+
+#' @rdname bag_models
+#' @export
+score2weight_mean <- function(x, col = "validation_score", score_threshold = 0.7){
+  x <- x[[weights_col]]
+  x <- mean(x, na.rm = TRUE)
+  x <- ifelse(x < score_threshold, 0, x) #set poorly validated models to zero
+  return(x)
+}
+
+#' @rdname bag_models
+#' @export
+score2weight_invmean <- function(x, col = "validation_score", score_threshold = 0.7){
+  x <- x[[weights_col]]
+  x <- 1/mean(1/x, na.rm = TRUE)
+  x <- ifelse(x < score_threshold, 0, x) #set poorly validated models to zero
   return(x)
 }
