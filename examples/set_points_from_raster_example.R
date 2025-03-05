cf94338413d6f7523d1417637f1f942690fbce1f
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2025-03-05 09:54:48 +0100

    removed NLMR from dependencies. set_points now does not have options with NLMR, but this function is minor in the package

--- examples/set_points_from_raster_example.R
+++ examples/set_points_from_raster_example.R
@@ -20,18 +20,3 @@ points(pts)
 r <- terra::rast(r)
 # points
 pts <- set_points_from_raster(r, n_features = 300)
-
-#-----
-# using NLMR
-library(NLMR)
-
-# example NLM
-set.seed(123)
-nlm1 <- NLMR::nlm_mpd(100, 100, 100, roughness = .5)
-
-# points
-pts <- set_points_from_raster(nlm1, n_features = 1000)
-
-# plot
-raster::plot(nlm1)
-points(pts)
