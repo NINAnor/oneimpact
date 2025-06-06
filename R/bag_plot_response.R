2bc5b40452d547ee8031a7919d809cf555924d9a
Bernardo Brandao Niebuhr <bernardo_brandaum@yahoo.com.br>
2025-06-06 11:25:36 +0200

    plot_response: change function after changes in predict.

--- R/bag_plot_response.R
+++ R/bag_plot_response.R
@@ -6,7 +6,7 @@
 #' confidence interval, computed from the weighted quantiles of the prediction. All other variables
 #' are kept constant, as defined by the `baseline` parameter.
 #'
-#' The function `plot_response` uses the `bag_predict` to produce the predictions.
+#' The function `plot_response` uses the `predict` to produce the predictions.
 #'
 #' @param x `[bag,list]` \cr A bag of models, resulting from a call to [oneimpact::bag_models()].
 #' @param dfvar `[data.frame]` \cr A data.frame with the values of the variables one wants to vary.
@@ -20,7 +20,8 @@
 #' @param zoi_shape `[character(1)="linear"]{"exp_decay", "gaussian_decay", "linear_decay", "threshold_decay"}` \cr
 #' Shape of the ZOI. Necessary to be specified to represent correctly the estimated ZOI of the
 #' preditor variables.
-#' @param wq_probs `[vector,numeric(3)=c(0.025, 0.5, 0.975)]` \cr a three element with lower, mid, and higher weighted quantiles to be computed
+#' @param wq_probs `[vector,numeric(3)=c(0.025, 0.5, 0.975)]` \cr A three element vector with lower,
+#' mid, and higher weighted quantiles to be computed.
 #' @param ci Should variation or confidence intervals be plotted?
 #'
 #' @export
@@ -36,6 +37,9 @@ plot_response <- function(x,
                           baseline = c("median", "mean", "zero")[1],
                           zoi = FALSE,
                           zoi_vals = c(100, 250, 500, 1000, 2500, 5000, 10000),
+                          type_feature = c("point", "line", "area")[1],
+                          resolution = 100, # resolution for the raster created in create_line_feature_zoi
+                          line_value = 1, # value sey to the linear inftastructure raster created in create_line_feature_zoi
                           ggplot = T,
                           plot_mean = TRUE,
                           plot_median = TRUE,
@@ -72,6 +76,9 @@ plot_response.bag <- function(x,
                               baseline = c("median", "mean", "zero")[1],
                               zoi = FALSE,
                               zoi_vals = c(100, 250, 500, 1000, 2500, 5000, 10000),
+                              type_feature = c("point", "line", "area")[1],
+                              resolution = 100,
+                              line_value = 1,
                               ggplot = T,
                               plot_mean = TRUE,
                               plot_median = TRUE,
@@ -90,81 +97,46 @@ plot_response.bag <- function(x,
                               alpha_ci = 0.5,
                               alpha_indiv = 0.3){
 
-  # baselines for plotting and predicting!! mean? median?
-  # define the baseline
-  bs <- baseline
-  if (baseline[1] == "median"){
-    baseline <- x$data_summary[rownames(x$data_summary) == "50%",]
-  } else {
-    if(baseline[1] == "mean") {
-      baseline <- x$data_summary[rownames(x$data_summary) == "mean",]
-    } else {
-      if (baseline[1] == "zero"){
-        baseline <- x$data_summary[5,]
-        baseline[1, 1+which(x$numeric_covs)] <- 0 # zero for numeric ones
-        baseline[1, 1+which(!x$numeric_covs)] <- sapply(unname(which(!x$numeric_covs)),
-                                                        function(z) levels(data[,names(x$numeric_covs[z])])[1])
-      } else {
-        stop(paste0("Invalid 'baseline' parameter: ", baseline, ". Pleas re-define."))
-      }
-    }
-  }
-
-  # is the variable a ZOI variable?
-  if (!zoi){
-
-    # new data
-    newdata <- baseline
-    newdata <- newdata[rep(1, nrow(dfvar)),]
-    newdata[,names(dfvar)] <- dfvar
-
-  } else{
-
-    # ZOI radii
-    zois <- names(x$data_summary)[grep(names(dfvar)[1], names(x$data_summary))]
-    # zois <- as.numeric(gsub(names(dfvar)[1], "", zois))
-    zoi_radii <- as.numeric(gsub("\\D", "", zois))
-
-    # compute ZOI for the intended distances
-    dfvar2 <- dfvar[,rep(1, length(zoi_radii)), drop = FALSE]
-    is_cumulative <- grepl(pattern = which_cumulative, zois)
-    dfvar2 <- as.data.frame(do.call("cbind", lapply(c(1:ncol(dfvar2)), function(i) {
-      n_feat <- ifelse(is_cumulative[i], n_features, 1)
-      n_feat*oneimpact::dist_decay(dfvar2[,i], radius = zoi_radii[i], type = zoi_shape) })))
-    names(dfvar2) <- zois#paste0(names(dfvar)[1], zoi_radii)
-
-    # cumulative vars
-    #dfvar2[,grepl("cumulative", colnames(dfvar2))]*100
-
-    # new data
-    newdata <- baseline
-    newdata <- newdata[rep(1, nrow(dfvar)),]
-    newdata[,names(dfvar2)] <- dfvar2
-    #if (ncol(dfvar)==2){newdata[,names(dfvar)[2]] <- dfvar[,2]}
-  }
-
-  # make sure prediction works even if categorical variables are constant
-  # get that from the summary instead of dat, where from? maybe a new object
-  for(i in which(!x$numeric_covs)) {
-    if(class(data[,names(x$numeric_covs[i])]) == "factor") {
-      newdata[,i+1] <- factor(newdata[,i+1], levels = levels(data[,names(x$numeric_covs[i])])) # factor
-    } else {
-      newdata[,i+1] <- factor(newdata[,i+1], levels = sort(unique(data[,names(x$numeric_covs[i])]))) # character
-    }
-  }
-
   # predict for new data set
 
   # predict only for those variables/coefs of interest for ZOI variables
   if(zoi) include <- colnames(dfvar) else include = "all"
+
   # predict
-  pred <- bag_predict(x, newdata, type = type, wMean = T, wq_probs = wq_probs, include = include)
+  pred <- predict(x,
+                      newdata = dfvar,
+                      data = data,
+                      type = type,
+                      wmean = TRUE,
+                      wq_probs = wq_probs,
+                      include = include,
+                      baseline = baseline,
+                      zoi = zoi,
+                      zoi_shape = zoi_shape,
+                      which_cumulative = which_cumulative,
+                      type_feature = type_feature,
+                      n_features = n_features,
+                      resolution = resolution,
+                      line_value = line_value)
 
   names(pred) <- c("lower", "mid", "higher", "mean")
   # predict for individual models
   if(indiv_pred) {
-    pred_indiv <- bag_predict(x, newdata, type = type, wMean = F, wq_probs = NULL, include = include)[, x$weights > 0]
-    # order(x$weights[x$weights > 0])
+    pred_indiv <- predict(x,
+                              newdata = dfvar,
+                              data = data,
+                              type = type,
+                              wmean = FALSE,
+                              wq_probs = NULL,
+                              include = include,
+                              baseline = baseline,
+                              zoi = zoi,
+                              zoi_shape = zoi_shape,
+                              which_cumulative = which_cumulative,
+                              type_feature = type_feature,
+                              n_features = n_features,
+                              resolution = resolution,
+                              line_value = line_value)[, x$weights > 0]
   }
 
 
@@ -242,51 +214,51 @@ plot_response.bag <- function(x,
     if (!zoi){
       # if (ncol(dfvar) == 1){
 
-        # data for plotting
-        df <- data.frame(x = newdata[,names(dfvar)[1]], y = pred$mid,
-                         y_lower = pred$lower, y_upper = pred$higher, y_mean = pred$mean)
-        # data for plotting individual models
-        if(indiv_pred) df_indiv <- tidyr::gather(data.frame(x = newdata[,names(dfvar)[1]], pred_indiv),
-                                                 key = "model", value = "y", -x)
+      # data for plotting
+      df <- data.frame(x = dfvar[,names(dfvar)[1]], y = pred$mid,
+                       y_lower = pred$lower, y_upper = pred$higher, y_mean = pred$mean)
+      # data for plotting individual models
+      if(indiv_pred) df_indiv <- tidyr::gather(data.frame(x = dfvar[,names(dfvar)[1]], pred_indiv),
+                                               key = "model", value = "y", -x)
 
-        plt <- ggplot2::ggplot(df)
-        if(class(dfvar[,1]) == "factor") {
-          if(indiv_pred) {
-            plt <- plt + ggplot2::geom_boxplot(ggplot2::aes(x = x,
-                                                            y = y),
-                                            data = df_indiv)
-          }
-        } else {
-          if(ci) {
-            plt <- plt + ggplot2::geom_ribbon(ggplot2::aes(x = x,
-                                                           ymin = y_lower,
-                                                           ymax = y_upper),
-                                              fill = col_ci,
-                                              alpha = alpha_ci)
-          } else {
-            if(indiv_pred) {
-              plt <- plt + ggplot2::geom_line(ggplot2::aes(x = x,
-                                                           y = y,
-                                                           group = model),
-                                              data = df_indiv,
-                                              color = col_indiv,
-                                              linewidth = linewidth_indiv,
-                                              alpha = alpha_indiv)
-            }
-          }
-          if(plot_median)
-            plt <- plt + ggplot2::geom_line(ggplot2::aes(x=x,
-                                                         y = y),
-                                            color = col_mean,
-                                            linewidth = linewidth_mean)
-          if(plot_mean)
-            plt <- plt + ggplot2::geom_line(ggplot2::aes(x=x,
-                                                         y = y_mean),
-                                            color = col_median,
-                                            linewidth = linewidth_median)
+      plt <- ggplot2::ggplot(df)
+      if(class(dfvar[,1]) == "factor") {
+        if(indiv_pred) {
+          plt <- plt + ggplot2::geom_boxplot(ggplot2::aes(x = x,
+                                                          y = y),
+                                             data = df_indiv)
         }
+      } else {
+        if(ci) {
+          plt <- plt + ggplot2::geom_ribbon(ggplot2::aes(x = x,
+                                                         ymin = y_lower,
+                                                         ymax = y_upper),
+                                            fill = col_ci,
+                                            alpha = alpha_ci)
+        } else {
+          if(indiv_pred) {
+            plt <- plt + ggplot2::geom_line(ggplot2::aes(x = x,
+                                                         y = y,
+                                                         group = model),
+                                            data = df_indiv,
+                                            color = col_indiv,
+                                            linewidth = linewidth_indiv,
+                                            alpha = alpha_indiv)
+          }
+        }
+        if(plot_median)
+          plt <- plt + ggplot2::geom_line(ggplot2::aes(x=x,
+                                                       y = y),
+                                          color = col_mean,
+                                          linewidth = linewidth_mean)
+        if(plot_mean)
+          plt <- plt + ggplot2::geom_line(ggplot2::aes(x=x,
+                                                       y = y_mean),
+                                          color = col_median,
+                                          linewidth = linewidth_median)
+      }
 
-        plt <- plt + ggplot2::labs(x = names(dfvar), y = y_lab, title = "")
+      plt <- plt + ggplot2::labs(x = names(dfvar), y = y_lab, title = "")
 
       # }
       # if (ncol(dfvar)==2){
@@ -303,7 +275,7 @@ plot_response.bag <- function(x,
       # }
     }
 
-    if (logx) { plt <- plt + scale_x_continuous(trans = 'log10') }
+    if (logx) { plt <- plt + ggplot2::scale_x_continuous(trans = 'log10') }
     if (!is.null(ylim)) { plt <- plt + ylim }
     return(plt + ggplot2::theme_minimal())
 
@@ -315,96 +287,3 @@ plot_response.bag <- function(x,
   }
 }
 
-# plot_response_multi_issf_lines <- function(x, dfvar, type=c("linear", "exponential")[2], prop_best=0.5, baseline=NULL, zoi=F, ggplot=T, logx=F, ylim=NULL){
-#   if (is.null(baseline)){
-#     baseline <- x$data_summary[3,]
-#   }
-#   if (baseline[1]=="zero"){
-#     baseline <- x$data_summary[3,]
-#     baseline[1,] <- 0
-#   }
-#   if (!zoi){
-#     newdata <- baseline
-#     newdata <- newdata[rep(1, nrow(dfvar)),]
-#     newdata[,names(dfvar)] <- dfvar
-#   } else{
-#     rescaling <- list()
-#     rescaling$point <- data.frame(zoi=c(100, 250, 500, 1000, 2500, 5000, 10000),
-#                                   scale=c(1.000000, 0.147929, 0.040000, 0.010000, 0.001600, 0.000400, 0.000100))
-#
-#     rescaling$line <- data.frame(zoi=c(100, 250, 500, 1000, 2500, 5000, 10000),
-#                                  scale=c(1.00000000, 0.53846154, 0.29066667, 0.14418182, 0.05788062, 0.02895027, 0.01448610))
-#
-#     zois <- names(x$data_summary)[grep(names(dfvar)[1], names(x$data_summary))]
-#     zois <- as.numeric(gsub(names(dfvar)[1], "", zois))
-#     dfvar2 <- dfvar[,rep(1, length(zois))]
-#     scal <- rescaling$point$scale[match(zois, rescaling$point$zoi)]
-#     dfvar2 <- as.data.frame(do.call("cbind", lapply(c(1:ncol(dfvar2)), function(i,dfvar2,zois,scal){apply(cbind((zois[i]-dfvar2[,i])/zois[i], 0), 1, max)*scal[i]}, dfvar2=dfvar2, zois=zois, scal=scal)))
-#     names(dfvar2) <- paste0(names(dfvar)[1], zois)
-#
-#     newdata <- baseline
-#     newdata <- newdata[rep(1, nrow(dfvar)),]
-#     newdata[,names(dfvar2)] <- dfvar2
-#     if (ncol(dfvar)==2){newdata[,names(dfvar)[2]] <- dfvar[,2]}
-#   }
-#
-#   pred_mean <- predict_multi_issf(x, newdata, type=type, wMean=T)
-#   pred <- predict_multi_issf(x, newdata, type=type, wMean=F)
-#   incl <- x$weights > quantile(x$weights, props=(1-prop_best/ncol(pred)))
-#   pred <- pred[,incl]
-#   dm <- dim(pred)
-#
-#   if (ggplot){
-#     require(ggplot2)
-#     if (zoi){
-#       if (ncol(dfvar)==1){
-#         preddf <- data.frame(x=rep(dfvar[,1], dm[2]), y=as.vector(pred), btstrp=rep(c(1:dm[2]), each=dm[1]))
-#         df <- data.frame(x=dfvar[,1], y=pred_mean[,1])
-#         plt <- ggplot(preddf) +
-#           geom_line(aes(x=x, y=y, group=btstrp), color='blue',alpha=5/dm[2]) +
-#           geom_line(data=df, aes(x=x, y=y), color='green') + ylim(min(df$y), max(df$y)) +
-#           labs(x="distance (m)",y="prediction",title = names(dfvar)[1])
-#       }
-#       if (ncol(dfvar)==2){
-#         df <- data.frame(x=dfvar[,1], grp=as.factor(newdata[,names(dfvar)[2]]), y=pred$mid, y_lower = pred$lower, y_upper=pred$higher, y2=pred$mean)
-#         plt <- ggplot(df, aes(x=x,
-#                               y=y, group=grp, color=grp, fill=grp)) +
-#           geom_ribbon(aes(ymin=y_lower,
-#                           ymax=y_upper), alpha=0.3, linetype=0) +
-#           geom_line() +
-#           geom_line(aes(x=x,
-#                         y=y2),
-#                     type=2) +
-#           labs(x="distance (m)",y="prediction",title = names(dfvar)[1], color=names(dfvar)[2], fill=names(dfvar)[2])
-#       }
-#     }
-#     if (!zoi){
-#       if (ncol(dfvar)==1){
-#         preddf <- data.frame(x=rep(newdata[,names(dfvar)[1]], dm[2]), y=as.vector(pred), btstrp=rep(c(1:dm[2]), each=dm[1]))
-#         df <- data.frame(x=newdata[,names(dfvar)[1]], y=pred_mean[,1])
-#         plt <- ggplot(preddf) +
-#           geom_line(aes(x=x, y=y, group=btstrp), color='blue',alpha=5/dm[2]) +
-#           geom_line(data=df, aes(x=x, y=y), color='green') + ylim(min(df$y), max(df$y)) +
-#           labs(x=names(dfvar),y="prediction",title = "")
-#       }
-#       if (ncol(dfvar)==2){
-#         df <- data.frame(x=newdata[,names(dfvar)[1]], grp=as.factor(newdata[,names(dfvar)[2]]), y=pred$mid, y_lower = pred$lower, y_upper=pred$higher, y2=pred$mean)
-#         plt <- ggplot(df, aes(x=x,
-#                               y=y, group=grp, color=grp, fill=grp)) +
-#           geom_ribbon(aes(ymin=y_lower,
-#                           ymax=y_upper), alpha=0.3, linetype=0) +
-#           geom_line() +
-#           geom_line(aes(x=x,
-#                         y=y2),
-#                     type=2) +
-#           labs(x=names(dfvar)[1],y="prediction",title = "", color=names(dfvar)[2], fill=names(dfvar)[2])
-#       }
-#     }
-#     if (logx){plt <- plt + scale_x_continuous(trans='log10')}
-#     if (!is.null(ylim)){plt <- plt + ylim}
-#     print(plt)
-#   } else{
-#     pred <- cbind(dfvar, pred)
-#     return(pred)
-#   }
-# }
